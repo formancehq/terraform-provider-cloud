c61bbf482a00b3f31f87e8ac74c6146c3f4999d2
David Ragot <35502263+Dav-14@users.noreply.github.com>
2025-06-11 16:35:19 +0200

    refactor: transport layer

--- pkg/testprovider/provider.go
+++ pkg/testprovider/provider.go
@@ -1,19 +1,22 @@
 package testprovider
 
 import (
+	"net/http"
+
 	"github.com/formancehq/go-libs/v3/logging"
 	"github.com/formancehq/terraform-provider-cloud/internal/server"
 	"github.com/formancehq/terraform-provider-cloud/pkg"
 	"github.com/hashicorp/terraform-plugin-framework/provider"
 )
 
-func NewCloudProvider(logger logging.Logger, version string, endpoint string, clientId string, clientSecret string, sdkFactory pkg.SDKFactory) func() provider.Provider {
+func NewCloudProvider(logger logging.Logger, version string, endpoint string, clientId string, clientSecret string, transport http.RoundTripper, sdkFactory pkg.SDKFactory) func() provider.Provider {
 	return server.New(
 		logger,
 		version,
 		endpoint,
 		clientId,
 		clientSecret,
+		transport,
 		sdkFactory,
 	)
 }
