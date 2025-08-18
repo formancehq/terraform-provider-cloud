f82b0df50137e12115fed1c37d7b34848db880a3
David Ragot <35502263+Dav-14@users.noreply.github.com>
2025-08-18 10:47:55 +0200

    feat: export schema

---
+++ examples/latest/main.tf
@@ -0,0 +1,9 @@
+terraform {
+  required_providers {
+    cloud = {
+      source = "formancehq/cloud"
+    }
+  }
+}
+
+provider "cloud" {}
--- main.go
+++ main.go
@@ -2,13 +2,16 @@
 // If you do not have Terraform installed, you can remove the formatting command, but it is suggested
 // to ensure the documentation is formatted properly.
 
-//go:generate rm -rf docs
-//go:generate mkdir docs
-//go:generate terraform fmt -recursive ./examples/
+// //go:generate rm -rf docs
+// //go:generate mkdir docs
+// //go:generate terraform fmt -recursive ./examples/
 
-// Generate documentation.
-//go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs@latest generate --provider-dir . -provider-name cloud
+// // Generate documentation.
+// //go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs@latest generate --provider-dir . -provider-name cloud
 
+// Generate terraform providers schema
+//
+//go:generate rm -rf ./schema && mkdir -p ./schema && cd examples/latest && terraform init -upgrade && terraform providers schema -json > ../../schema/providers.json
 package main
 
 import "github.com/formancehq/terraform-provider-cloud/cmd"
